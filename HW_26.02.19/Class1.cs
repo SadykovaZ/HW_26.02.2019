ded9a6153551e4d03a95eb8d33cd258d7185880e
Жанара <@DESKTOP-O9KC10I>
2019-03-04 15:22:29 +0600

    Add project files.

---
+++ HW_26.02.19/Class1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_26._02._19
+{
+    public enum Events { Пожар, ДеньГорода, Ограбление }
+    public delegate void DelEvents(Events e);
+    public class CityEvents
+    {
+        Random rnd = new Random();
+        public event DelEvents delEv;
+        public void StartEvent()
+        {
+            Events t = (Events)rnd.Next(Enum.GetNames(typeof(Events)).Length);            
+            System.Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(t);
+            System.Console.ForegroundColor = ConsoleColor.White;
+
+            delEv(t);
+        }
+    }
+    public class FireMan
+    {
+        public void Message(Events ev)
+        {
+            System.Console.ForegroundColor = ConsoleColor.Red;
+            if (ev == Events.Пожар)
+                Console.WriteLine("Пожарные тушат огонь");
+            else if (ev == Events.ДеньГорода)
+                Console.WriteLine("Пожарные отдыхают");
+            else if (ev == Events.Ограбление)
+                Console.WriteLine("Пожарные ничего не делают");     
+                System.Console.ForegroundColor = ConsoleColor.White;
+
+        }
+    }
+    public class Managers
+    {
+        public void Message(Events ev)
+        {
+            System.Console.ForegroundColor = ConsoleColor.Green;
+            if (ev == Events.Пожар)
+                Console.WriteLine("Менеджеры сидят дома");
+            else if (ev == Events.ДеньГорода)
+                Console.WriteLine("Менеджеры организовывают день города");
+            else if (ev == Events.Ограбление)
+                Console.WriteLine("Менеджеры закрываются в доме");
+            System.Console.ForegroundColor = ConsoleColor.White;
+
+        }
+    }
+    public class Police
+    {
+        public void Message(Events ev)
+        {
+            System.Console.ForegroundColor = ConsoleColor.Blue;
+            if (ev == Events.Пожар)
+                Console.WriteLine("Полицейские едут на помощь");
+            else if (ev == Events.ДеньГорода)
+                Console.WriteLine("Полицейские отдыхают");
+            else if (ev == Events.Ограбление)
+                Console.WriteLine("Полицейские ищут преступника");
+            System.Console.ForegroundColor = ConsoleColor.White;
+
+        }
+    }
+}
---
+++ HW_26.02.19/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_26._02._19
+{
+    public class Program
+    {
+        delegate double Mathematic(double a);
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1-Плохая служба, 2-Город и его службы: ");
+            int choice = int.Parse(Console.ReadLine());
+            if (choice == 1)
+            {
+                Ex01();
+            }
+            else if (choice == 2)
+            {
+                CityEvents city = new CityEvents();
+                FireMan fireM = new FireMan();
+                Managers manager = new Managers();
+                Police police = new Police();
+
+                city.delEv += police.Message;
+                city.delEv += fireM.Message;
+                city.delEv += manager.Message;
+
+                for (int i = 0; i < 4; i++)
+                {
+                    city.StartEvent();
+                    Console.WriteLine("\n");
+                }
+            }
+        }
+
+        public static void Ex01()
+        {
+            Mathematic m = POW;
+            m += SQRT;
+            m += MultByTwo;
+
+            Random rnd = new Random();
+
+            for (double i = 0; i < 3; i++)
+            {
+                double a = rnd.Next(1, 10);
+
+                Console.WriteLine("a = {0}",a);
+                foreach (Mathematic mat in m.GetInvocationList())
+                {
+                    try
+                    {
+                        double res = mat.Invoke(a);
+                        Console.WriteLine(mat.Method.Name + ": " + res);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(mat.Method.Name + ": " + ex.Message);
+                    }
+                }
+                Console.WriteLine("\n");
+            }
+        }
+        static double POW(double x)
+        {
+            return x * x; ;
+        }
+        static double SQRT(double x)
+        {
+            return Math.Sqrt(x);
+        }
+        static double MultByTwo(double x)
+        {
+            return x * 2;
+        }
+
+    }
+
+}
